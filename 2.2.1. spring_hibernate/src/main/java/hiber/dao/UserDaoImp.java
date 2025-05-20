457fdf07df9dd56d8878a9dddd2680087c759d35
Evgen <vertumnartist@bk.ru>
2025-05-20 09:55:27 +0300

    задача 2.2.1 Hibernate и ORM

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -6,24 +6,37 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Repository
 public class UserDaoImp implements UserDao {
 
-   @Autowired
-   private SessionFactory sessionFactory;
+    @Autowired
+    private SessionFactory sessionFactory;
 
-   @Override
-   public void add(User user) {
-      sessionFactory.getCurrentSession().save(user);
-   }
+    @Override
+    public void add(User user) {
+        sessionFactory.getCurrentSession().save(user);
+    }
 
-   @Override
-   @SuppressWarnings("unchecked")
-   public List<User> listUsers() {
-      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
-      return query.getResultList();
-   }
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<User> listUsers() {
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User");
+        return query.getResultList();
+    }
+
+    @Override
+    @Transactional
+    public List<User> listUsersByCarModelAndSeries(String carModel, int carSeries) {
+        return sessionFactory.getCurrentSession().createQuery(
+                        "SELECT u FROM User u " +
+                                "JOIN u.car c " +
+                                "WHERE c.model = :carModel AND c.series = :carSeries", User.class)
+                .setParameter("carModel", carModel)
+                .setParameter("carSeries", carSeries)
+                .getResultList();
+    }
 
 }
